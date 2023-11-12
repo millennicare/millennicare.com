5e98b3a806c2c453de355bba8ae4ad1e3c5efef4
Steven Arce <stevenmatthew.arce@gmail.com>
2023-11-12 02:05:24 -0800

    added privacy docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,65 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "~/components/ui/button";
-
-const navLinks = [
-  {
-    title: "About",
-    href: "#about",
-  },
-  {
-    title: "Our Commitment",
-    href: "#commitment",
-  },
-  {
-    title: "Meet Our Team",
-    href: "#team",
-  },
-];
+import Navbar from "./_components/Navbar";
 
 export default function Page() {
-  const navItems = navLinks.map(({ href, title }) => {
-    return (
-      <div key={href}>
-        <Link href={href}>
-          <Button variant="link" size="lg" className="text-lg">
-            {title}
-          </Button>
-        </Link>
-      </div>
-    );
-  });
-
   return (
     <div className="flex flex-col">
-      <div
-        id="navbar"
-        className="flex w-full items-center justify-between bg-palecream px-4 py-6"
-      >
-        <Image
-          src="/millennicare_logo.png"
-          height={50}
-          width={50}
-          alt="Millennicare Logo"
-          className="block md:hidden"
-          priority={true}
-        />
-
-        <Image
-          className="hidden md:block"
-          src="/millennicare_logo_with_text.svg"
-          height={500}
-          width={300}
-          alt="Millennicare Logo"
-        />
-
-        <div className="flex">
-          {navItems}
-          <Button variant="secondary" size="lg" className="text-lg text-white">
-            <Link href="/auth/login">Login</Link>
-          </Button>
-        </div>
-      </div>
+      <Navbar />
 
       <div
         id="hero"
@@ -123,6 +70,92 @@ export default function Page() {
             to become a part of the first 100 founding families to be eligible
             for subsidy!
           </p>
+        </div>
+      </div>
+
+      <div
+        id="commitment"
+        className="flex h-full flex-col justify-center bg-palecream py-12 lg:h-[65vh] lg:px-36"
+      >
+        <h1 className="justify-center text-center text-4xl text-primary md:m-8">
+          Our Commitment
+        </h1>
+
+        <div className="flex flex-col items-center justify-center sm:flex-row md:justify-evenly">
+          <div className="flex w-full flex-col items-center justify-center space-y-4 md:w-1/3">
+            <Image
+              src="/safe-icon.png"
+              alt="safe icon"
+              height={80}
+              width={80}
+            />
+            <h3>SAFE</h3>
+            <p className="px-5 text-center text-base">
+              Book a childcare session with an ease of mind. Our team makes sure
+              that all of our caregivers are background checked, and we comply
+              to DC licensing and compliance guidelines.
+            </p>
+          </div>
+          <div className="mx-8 flex w-full flex-col items-center justify-center space-y-4 md:w-1/3">
+            <Image
+              src="/childcare-icon.png"
+              alt="childcare icon"
+              height={80}
+              width={80}
+            />
+            <h3 className="text-center">AFFORDABLE CHILDCARE</h3>
+            <p className="px-5 text-center text-base">
+              Pay a low-monthly fee to access quality and affordable childcare
+              service. We are aligned with the state and federal government
+              initatives to reduce childcare cost from $1600 per month to only
+              $500 per month.
+            </p>
+          </div>
+          <div className="flex w-full flex-col items-center justify-center space-y-4 md:w-1/3">
+            <Image
+              src="/for-you-icon.png"
+              alt="for you icon"
+              height={80}
+              width={80}
+            />
+            <h3>FOR YOU</h3>
+            <p className="px-5 text-center text-base">
+              Designed for working women, families of all kinds, as well as
+              companies offering fringe benefits to increase retention and
+              employee morale. MillenniCare addresses the flaws in our current
+              fractured system.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="footer"
+        className="flex w-full flex-col content-around items-center bg-palecream py-10"
+      >
+        <Image
+          src="/millennicare_logo_with_text.svg"
+          alt="Millennicare logo"
+          height={200}
+          width={200}
+        />
+
+        <p className="pt-2 text-sm">Copyright © 2023 MillenniCare, Inc.</p>
+        {/* Footer Links */}
+        <div className="flex w-[390px] flex-col items-center justify-between pt-2 min-[420px]:flex-row">
+          <Link href="/eula">
+            <p className="hover:text-primary">EULA</p>
+          </Link>
+          <p className="max-[420px]:hidden">|</p>
+          <Link href="/privacy-policy">
+            <p className="hover:text-primary max-[420px]:py-2">
+              Privacy Policy
+            </p>
+          </Link>
+          <p className="hover:text-primary max-[420px]:hidden">|</p>
+          <Link href="/contact-us">
+            <p>Contact Us</p>
+          </Link>
         </div>
       </div>
     </div>
