54bac8fab93690c785e5428949e872396b12a224
Steven Arce <stevenmatthew.arce@gmail.com>
2023-12-26 11:01:01 -0800

    fixed next config to alow s3 images

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -11,6 +11,17 @@ const config = {
   ],
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          process.env.NODE_ENV === "development"
+            ? "millennicare-dev.s3.us-east-1.amazonaws.com"
+            : "millennicare-app-files.s3.us-east-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default config;
