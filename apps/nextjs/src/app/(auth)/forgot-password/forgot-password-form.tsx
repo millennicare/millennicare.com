10445fee3adf2d5e82ac8791dc8414c669366256
Steven Arce <stevenmatthew.arce@gmail.com>
2024-01-30 17:51:55 -0800

    Added conditional rendering for forgt password form to show different states of before and after the email was sent

--- apps/nextjs/src/app/(auth)/forgot-password/forgot-password-form.tsx
+++ apps/nextjs/src/app/(auth)/forgot-password/forgot-password-form.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { useState } from "react";
+import Link from "next/link";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
+import { Button } from "@millennicare/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -27,6 +29,7 @@ const formSchema = z.object({
 });
 
 export default function ForgotPasswordForm() {
+  const [emailSent, setEmailSent] = useState(false);
   const form = useForm({
     schema: formSchema,
     defaultValues: {
@@ -37,9 +40,8 @@ export default function ForgotPasswordForm() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-      const response = await sendResetPasswordEmail(values.email);
-      console.log(response);
-      toast.success(response.message);
+      await sendResetPasswordEmail(values.email);
+      setEmailSent(true);
     } catch (error) {
       if (error instanceof TRPCError) {
         toast.error(error.message);
@@ -48,38 +50,54 @@ export default function ForgotPasswordForm() {
       } else toast.error("Something went wrong, please try again later.");
     }
   }
+
   return (
     <div className="flex w-full flex-col items-center space-y-4">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex w-full max-w-md flex-col space-y-4 rounded-lg border px-4 py-2 md:w-1/2"
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="example@email.com" />
-                </FormControl>
-                <FormDescription>
-                  Enter the email address associated with your account and we'll
-                  send you an email with instructions to reset your password.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+      {emailSent ? (
+        <span className="flex w-full max-w-md flex-col space-y-4 rounded-lg border px-4 py-2 md:w-1/2">
+          <p className="text-sm">
+            An email is on it&apos;s way to {form.getValues("email")} with
+            instructions to reset your password.
+            <br />
+            <br />
+            If you don't receive an email soon, check that the email address you
+            entered is correct, check your spam folder or reach out to support
+            if the issue persists.
+          </p>
 
-          <SubmitButton
-            value="Send reset instructions"
-            className="w-full"
-            error={!form.formState.errors}
-          />
-        </form>
-      </Form>
+          <Button asChild>
+            <Link href="/sign-in">Back to sign in</Link>
+          </Button>
+        </span>
+      ) : (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex w-full max-w-md flex-col space-y-4 rounded-lg border px-4 py-2 md:w-1/2"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="example@email.com" />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <SubmitButton
+              value="Send reset instructions"
+              className="w-full"
+              error={!form.formState.errors}
+            />
+          </form>
+        </Form>
+      )}
     </div>
   );
 }
