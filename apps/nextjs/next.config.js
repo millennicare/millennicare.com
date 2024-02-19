baa700a7eaafde6bfed9f96c40dacdd44f6633f4
Steven Arce <stevenmatthew.arce@gmail.com>
2024-02-19 11:48:08 -0800

    changed next config

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -4,7 +4,6 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: "standalone",
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: [
     "@millennicare/auth",
