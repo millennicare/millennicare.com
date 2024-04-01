43ccbeca3a397ffbac0d3996699c1760af0596b7
Steven Arce <stevenmatthew.arce@gmail.com>
2024-03-31 23:24:47 -0700

    changed db index.ts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -30,11 +30,7 @@ export const schema = {
 };
 
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.POSTGRES_PORT!),
-  database: process.env.POSTGRES_DB,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export { pgTable as tableCreator } from "./schema/_table";
