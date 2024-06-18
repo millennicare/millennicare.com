36fe19b2f8f5c289414cf1dd97b5bfca9780e53b
Steven Arce <stevenmatthew.arce@gmail.com>
2024-06-18 16:59:19 -0700

    added new validator

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -8,3 +8,23 @@ export const signInSchema = object({
     message: "Password is required",
   }),
 });
+
+export const signUpSchema = object({
+  email: string({ required_error: "Email is required" })
+    .email()
+    .min(1, { message: "Email is required" }),
+  password: string()
+    .regex(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\w~@#$%^&*+=`|{}:;!.?\"()\[\]-]{8,32}$/, // eslint-disable-line
+      {
+        message:
+          "Password must be minimum 8 characters and at least one uppercase letter, one lowercase letter, and one number.",
+      },
+    )
+    .min(8, { message: "Password must be between 8 and 32 characters." })
+    .max(32, { message: "Password must be between 8 and 32 characters." }),
+  confirm: string(),
+}).refine((data) => data.password === data.confirm, {
+  path: ["confirm"],
+  message: "Passwords do not match",
+});
